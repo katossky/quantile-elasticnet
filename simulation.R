11564689f57d314a6d8fd4dee697e0e72de7109a
My Name <lucie.mc.tournier@gmail.com>
2023-01-10 12:32:12 +0100

    gibbs update fct partie 1

--- simulation.R
+++ simulation.R
@@ -1,12 +1,12 @@
-clear()
+rm()
 
 ########################### Simulation des données #############################
 
 ## choix des paramètres
 n = 500
-tau0 <- 2
+tau <- 2
 beta0 <- c(3, 3, 0, 3, 3)
-l <- length(beta0)
+p <- length(beta0)
 
 ## quantile 
 theta <- 0.5
@@ -15,11 +15,12 @@ xi2 <- sqrt(2 / (theta * (1-theta)))
 
 ## simulation des variables v, z, u, x, et y
 v <- rexp(n)
-v_bar <- v / tau0
+v_bar <- v / tau
 z <- rnorm(n)
-u <- xi1 * v_bar + xi2 * sqrt(v_bar / tau0) * z
-x <- array(rnorm(n*l), c(l, n))
-y <- t(x) %*% beta0 + u
+u <- xi1 * v_bar + xi2 * sqrt(v_bar / tau) * z
+
+x <- matrix(rnorm(n*p), nrow=n, ncol=p)
+y <- x %*% beta0 + u
 
 ########################### Lois Conditionnelles ###############################
 
@@ -33,15 +34,31 @@ d2 <- 1
 # initialisation des paramètres 
 tau <- rgamma(1, shape=a, rate=b)
 eta1 <- rgamma(1, shape=c1, rate=d1)
-eta2 <- rgamma(1, sgape=c2, rate=d2)
-beta <- rep(0, l)
+eta2 <- rgamma(1, shape=c2, rate=d2)
+eta1_bar <- (eta1**2) / (4*eta2)
+beta <- rep(0, p)
 
+r_gibbs <- 1000
 
-
-#x est un vecteur contenant une série de valeurs
-cv<-function(x){
-  moy<-mean(x) # moyenne de x
-  s<-sd(x)# ecart type de x
-  rslt<-s/moy # calcul du CV
-  rslt #la fonction retourne le résultat
+Gibbs_update <- function(x, y, tau, xi1, xi2, eta1_bar, eta2, beta){
+    # v_bar 
+    lambda <- tau * (y - x%*%beta)**2 / (xi2 ** 2)
+    mu2 <- lambda / ( (tau * xi1**2)/xi2**2 + 2*tau ) 
+    mu <- sqrt(mu2)
+    v_bar <- rinvgauss(1, mean = mu, shape = lambda)
+    
+    # y_bar 
+    y_bar <- matrix(nrow=n, ncol=p)
+    for(k in 1:p){
+        y_bar[,k] <- y - xi1*v_bar - sum( t( (t(x)*beta)[-k] ) )
+    }
+    
+    # t
+    t <- rep(2, p) ### A completer
+    
+    # sigma
+    #sigma_inv2[k] <- tau * xi2**(-2) * sum(x[,k] * v_bar**(-1)) + 2 * eta2 * t[k]*(t[k] - 1)**(-1)
+    sigma_inv2 <- tau * xi2**(-2) * colSums(x * v_bar**(-1)) + 2 * eta2 * t*(t - 1)**(-1)
+  
+    eta1_bar; eta2; beta
 }
